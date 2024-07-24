8a75a39f21cb8698c56f2c563c0632385a255216
sasha-cattaneo <s.cattaneo29@studenti.unibg.it>
2024-07-24 16:01:43 +0200

    Update php scripts
    Now allow export of multiple tables

---
+++ scripts_php/export_tables-data.php
@@ -0,0 +1,51 @@
+<?php
+include("config.php");
+$connessione = mysqli_connect($host, $username, $password, $database);
+
+if(!$connessione)
+    die("Errore di connessione: " . $connessione->connect_error);
+
+$table_list = "";
+if($_GET['table']=="" || !isset($_GET['table'])){
+    if($_POST['table']=="" || !isset($_POST['table'])){
+        die("ERRORE GET['table'] vuoto");
+    }else{
+        $table_list = $_POST['table'];
+    }
+}else{
+    $table_list = $_GET['table'];
+}
+$tables = array();
+foreach($table_list as $i => $value){
+    $q = "SELECT * FROM ".$value;
+
+    $query = $connessione->prepare($q);
+    if($query == false)
+        die("Errore query:".$q);
+    $query->execute();
+    //print($query);
+    //$result = mysqli_query($connessione, $query);
+    $result = $query->get_result();
+
+    $tables[] = tableToArray($value, $result);
+}
+mysqli_close($connessione);
+
+header('Content-Type: application/json; charset=utf-8');
+echo json_encode($tables);
+?>
+
+<?php
+function tableToArray ($table, $result){
+    
+    $rows = array();
+
+    while($row = $result->fetch_assoc()) {
+        $rows[] = $row;
+    }
+
+    $json_table = array($table => $rows);
+
+    return $json_table;
+}
+?>
