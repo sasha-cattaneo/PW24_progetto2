70a9da19d09997705f7c5e5c80a41810c6178608
sasha-cattaneo <s.cattaneo29@studenti.unibg.it>
2024-07-27 17:04:15 +0200

    Final PHP scripts, with comments

--- scripts_php/export_tables-structure.php
+++ scripts_php/export_tables-structure.php
@@ -1,20 +1,27 @@
 <?php
+// Creo connessione al DB
 include("config.php");
 $connessione = mysqli_connect($host, $username, $password, $database);
 
+// In caso di errore di connessione termino e restituisco l'errore
 if(!$connessione)
     die("Errore di connessione: " . $connessione->connect_error);
 
+// Controllo se sono stati passati, tramite GET o POST, i parametri necessari
+// cioè la lista di tabelle da esportare
+// Salvo la lista in una variabile
 $table_list = "";
     if($_GET['table']=="" || !isset($_GET['table'])){
         if($_POST['table']=="" || !isset($_POST['table'])){
-            die("ERRORE GET['table'] vuoto");
+            die("ERRORE GET['table'] e POST['table'] vuoto");
         }else{
             $table_list = $_POST['table'];
         }
     }else{
         $table_list = $_GET['table'];
     }
+// Per ogni tabella richiesta chiamo una query per ottenere la sua struttura
+// Salvo il risultato in un array
 $tables = array();
 foreach($table_list as $i => $value){
     $q = "DESCRIBE ".$value;
@@ -27,6 +34,7 @@ foreach($table_list as $i => $value){
     //$result = mysqli_query($connessione, $query);
     $result = $query->get_result();
 
+    // Creo un array chiave:valore dal nome della tabella e il risultato della query 
     $tables[] = tableToArray($value, $result);
 }
 mysqli_close($connessione);
@@ -36,6 +44,7 @@ echo json_encode($tables);
 ?>
 
 <?php
+// Creo un array chiave:valore
 function tableToArray ($table, $result){
     
     $rows = array();
